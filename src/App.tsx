eb4f40f592bdd715f2d5499de4a01de9aad488e7
Krystof <kipash612@gmail.com>
2023-08-30 15:47:09 +0200

    first mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React, { useRef } from 'react'
 import { NeedleEngine } from './NeedleEngine';
-
+import { ContextRegistry, GameObject, Rigidbody } from '@needle-tools/engine';
+import * as THREE from 'three';
+import { randFloat } from 'three/src/math/MathUtils';
 
 const App = () => {
-  const goToNeedle = () => {
-    window.location.href = "https://needle.tools";
+
+  // Fetch rigidbodies from needle
+  var bodies: Rigidbody[] = []
+  ContextRegistry.addContextCreatedCallback((_context) => {
+    bodies = GameObject.findObjectsOfType(Rigidbody);
+  })
+
+  const performJump = () => { 
+    const vec = new THREE.Vector3(0, 1, 0);
+    bodies.forEach((body) => {
+      vec.set(randFloat(-1, 1), randFloat(0.5, 1), randFloat(-1, 1));
+      body.applyImpulse(vec);
+    })
   };
 
   return (
     <>
-      <NeedleEngine>
-        <button type="button" onClick={goToNeedle}>
-          Visit Needle 🌵
-        </button>
-      </NeedleEngine>
+      <div id="container" className="needle-container">
+        <NeedleEngine style={{position: "relative", display: "flex"}}>
+          <div style={{width: "100%", heigth: "100%"}}>
+            <button type="button" onClick={performJump} className="jump-button">Squish!</button>
+          </div>
+        </NeedleEngine>
+      </div>
+      <div style={{height: "30vh", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column"}}>
+        <h1 style={{width: "max(80%, 500px)", textAlign: "center"}}><a id="needle" href="https://needle.tools" target="_blank">Needle Engine</a> in <a id="react" href="https://github.com/needle-engine/react-sample" target="_blank">React</a></h1>  
+        <h2 style={{width: "min(80%, 800px)", textAlign: "center"}}>The canvas can be transparent allowing for a seamless experience.<br/>That's done by setting the camera's "Clear mode" to "Don't Clear".</h2>
+      </div>
     </>
   );
 };
--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,15 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/ReactSample.glb?v=1693235724067");
+needle_exported_files.push("assets/ReactSample.glb?v=1693401243532");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1693235724067");
+		needleEngine.setAttribute("hash", "1693401243532");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
-
-console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.12\.0-pre\.2");
