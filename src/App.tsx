600be840d37846f0738161f60be78fdbc4981354
Marcel Wiessler <marcel@gaisterhand.de>
2023-09-26 12:54:02 +0200

    add @mui/material ui library for demonstration

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { useRef } from 'react'
 import { NeedleEngine } from './NeedleEngine';
 import { ContextRegistry, GameObject, Rigidbody } from '@needle-tools/engine';
 import { Vector3 } from 'three';
+import Button from '@mui/material/Button'
+import { Modal } from '@mui/material';
+import BasicModal from './Modal';
 
 function randFloat(min: number, max: number) {
   return Math.random() * (max - min) + min;
@@ -27,14 +30,15 @@ const App = () => {
     <>
       <div id="container" className="needle-container">
         <NeedleEngine style={{ position: "relative", display: "flex" }} loading-style="light">
-          <div style={{ width: "100%", heigth: "100%" }}>
-            <button type="button" onClick={performJump} className="jump-button">Squish!</button>
+          <div style={{ width: "100%", height: "100%" }}>
+            <Button variant="contained" onClick={performJump} className="jump-button">Squish!</Button>
           </div>
         </NeedleEngine>
       </div>
       <div style={{ height: "30vh", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column" }}>
         <h1 style={{ width: "max(80%, 500px)", textAlign: "center" }}><a id="needle" href="https://needle.tools" target="_blank">Needle Engine</a> in <a id="react" href="https://github.com/needle-engine/react-sample" target="_blank">React</a></h1>
         <h2 style={{ width: "min(80%, 800px)", textAlign: "center" }}>The canvas can be transparent allowing for a seamless experience.<br />That's done by setting the camera's "Clear mode" to "Don't Clear".</h2>
+        <BasicModal />
       </div>
     </>
   );
