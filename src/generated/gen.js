255329ccd6f52e02ffbca50fa45db6ab4f3db64d
Krystof <kipash612@gmail.com>
2024-04-04 14:40:57 +0200

    update sample (package.json, scene files)

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -1,14 +1,14 @@
-﻿import "./register_types.ts"
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/ReactSample.glb?v=1694716553828");
+needle_exported_files.push("assets/ReactSample.glb?v=1712230995150");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1694716553828");
+		needleEngine.setAttribute("hash", "1712230995150");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
